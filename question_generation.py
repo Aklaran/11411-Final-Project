0252b593aec0c20feb5e955ff6d3512a3962337b
Bo Tembunkiart <botbunk@gmail.com>
2019-11-05 18:10:20 -0500

    reworks to account for noun chunks

--- question_generation.py
+++ question_generation.py
@@ -2,54 +2,69 @@
 
 from copy import deepcopy
 import spacy_tokenization as st
+import spacy
 
 class QuestionGenerator:
 
     def __init__(self, preprocessor):
-        tagged_text = preprocessor.processed_doc
+        tagged_text = preprocessor.doc
 
-        wh_questions = self.generateWhQuestions(tagged_text)
+        self.wh_questions = self.generateWhQuestions(tagged_text)
 
-        print(wh_questions)
-
-    def generateWhQuestions(self, taggedText):
+    def generateWhQuestions(self, tagged_text):
         output = []
 
-        for sentence in taggedText:
-            question = False
+        for sentence in tagged_text.sents:
+            is_question = False
+            question = []
+            main_verb = None
+            main_verb_found = False
 
-            for possible_subject in sentence:
-                if possible_subject['dep'] == 'nsubj' and possible_subject['headPos'] == 'VERB':
-                    # print(possible_subject['text'] + ' ' + possible_subject['ent_type'])
+            for token in sentence:
+                # looking for the nominal subject of the sentence...
+                if token.dep_ == 'nsubj' and token.head.pos_ == 'VERB':
+                    # try to get a wh- pronoun for the subject
+                    possible_pronoun = self.replaceWhSubject(token)
 
-                    # replace the subject noun chunk with the appropriate wh- pronoun
-                    possible_pronoun = self.replaceWhSubject(possible_subject)
-
+                    # if a pronoun exists, replace the word, mark this as a valid question and begin constructing the question
                     if possible_pronoun:
-                        question = True
-                        possible_subject['text'] = possible_pronoun
+                        is_question = True
+                        main_verb = token.head
+                        question.append(possible_pronoun) # TODO:later, will want to append whole token for synonyms. for now, just string.
                 
-            # reconstruct the question with a question mark and add it to the output
-            if question:
-                out = " ".join([x['text'] for x in sentence[:-1]])
+                # if a nominal subject has been found, start constructing the rest of the sentence
+                if is_question:
+                    # waiting till we see the main verb because we don't want to append any extra words from the nsubj's noun chunk
+                    if main_verb and token == main_verb:
+                        main_verb_found = True
+                    
+                    # once we've found the main verb, begin appending tokens 
+                    if main_verb_found:
+                        question.append(token.text)
+            
+            # construct the question string and add to output
+            if len(question) > 0:
+                out = " ".join([x for x in question[:-1]]) # indexing till -1 to avoid final punctuation
                 out += "?"
+                out = out.capitalize()
 
                 output.append(out)
         
         return output
 
     def replaceWhSubject(self, subj):
-        # create a dictionary to map from entity types to wh- pronouns
+        # dictionary to map from entity types to wh- pronouns
         switcher = {
             'PERSON': 'who',
             'LOC': 'where',
             'ORG': 'what'
         }
         
-        return switcher.get(subj['ent_type'], None)           
+        return switcher.get(subj.ent_type_, None)           
 
 def main():
     qg = QuestionGenerator(st.Preprocessor())
+    print(qg.wh_questions)
 
 if __name__ == "__main__":    
     main()
--- spacy_tokenization.py
+++ spacy_tokenization.py
@@ -10,10 +10,10 @@ class Preprocessor:
     def __init__(self):
         nlp = spacy.load("en_core_web_sm")
         neuralcoref.add_to_pipe(nlp)
-        doc = nlp("Apple isn't looking at buying U.K. startup for $1 billion.")
+        self.doc = nlp("Guy Fawkes, also known as Guido Fawkes while fighting for the Spanish, was a member of a group of provincial English Catholics who planned the failed Gunpowder Plot of 1605. He was born and educated in York; his father died when Fawkes was eight years old, after which his mother married a recusant Catholic. Fawkes converted to Catholicism and left for mainland Europe, where he fought for Catholic Spain in the Eighty Years' War against Protestant Dutch reformers in the Low Countries. He travelled to Spain to seek support for a Catholic rebellion in England without success. He later met Thomas Wintour, with whom he returned to England. Wintour introduced him to Robert Catesby, who planned to assassinate King James I and restore a Catholic monarch to the throne. The plotters leased an undercroft beneath the House of Lords; Fawkes was placed in charge of the gunpowder which they stockpiled there. The authorities were prompted by an anonymous letter to search Westminster Palace during the early hours of 5 November, and they found Fawkes guarding the explosives. He was questioned and tortured over the next few days and confessed to wanting to blow up the House of Lords.")
 
-        self.processed_doc = self.process(doc)
-        self.coref = doc._.coref_clusters
+        self.processed_doc = self.process(self.doc)
+        self.coref = self.doc._.coref_clusters
 
     #   print(self.processed_doc)
         # print(self.coref)
