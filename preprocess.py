feb474f16cd5c08c126dec8d52449208e2af2361
Ethan Cordaro <ethancordaro@Ethans-MBP-5.fios-router.home>
2019-11-06 17:39:49 -0500

    ethan's updated preprocessor

--- preprocess.py
+++ preprocess.py
@@ -30,6 +30,25 @@ class Preprocessor:
     def process(self, doc):
 
         preprocessed_doc = []
+
+        #Noun chunks are “base noun phrases” – flat phrases that have a noun as their 
+        #head. You can think of noun chunks as a noun plus the words describing the 
+        #noun – for example, “the lavish green grass” or “the world’s largest tech fund”
+        
+        #https://spacy.io/usage/linguistic-features#dependency-parse
+
+        #can also get list of chunks: chunks = list(doc.noun_chunks)
+        chunk_list = []
+        for chunk in doc.noun_chunks:
+            chunkdict = {
+                'chunk_test': chunk.text,
+                'chunk_root_text': chunk.root.text,
+                #string value .dep_, hash value .dep
+                'chunk_root_dep': chunk.root.dep_,
+                'chunk_root_head': chunk.root.head.text
+                }
+            chunk_list.append(chunkdict)
+
         for sent in doc.sents:
 
             print(sent.text)
@@ -45,6 +64,7 @@ class Preprocessor:
                 # determine entity type and iob
                 ent_iob = token.ent_iob_
                 ent_type = token.ent_type_
+                ##should this be done with ent.label_
 
                 # build dictionary
                 token_dict = {
@@ -58,10 +78,15 @@ class Preprocessor:
                     'stop': token.is_stop,
                     'ent_iob': ent_iob,
                     'ent_type': ent_type,
-                    'coref': coref
+                    'coref': coref,
+
+                    ##for dependency parse: need to consider merging chunk dict as well
+                    'head_text': token.head.text,
+                    'head_pos': token.head,pos_
                     }
                     
                 sentence.append(token_dict)
+                #get whole phrase by syntactic head with token.subtree
 
                 # sample coref resolution (print main coref)
                 if coref:
@@ -72,6 +97,8 @@ class Preprocessor:
             preprocessed_doc.append(sentence)
         
         return preprocessed_doc
+
+        #at end check for doc.is_parsed
 
 # local testing (delete for final version)
 prep = Preprocessor()
