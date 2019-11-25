0042c7ee0f7dc0388146aba402e7b67fd0854720
Bo Tembunkiart <botbunk@gmail.com>
2019-11-25 11:54:41 -0500

    changes to predicates

--- predicate_framework.py
+++ predicate_framework.py
@@ -2,8 +2,13 @@
 
 from utils import *
 
+# pattern to match for simple predicates
 PRED_PATTERN = ['NP', 'VP', '.']
 
+# Berkeley Neural Parser token tags that we will include in noun phrases
+NOUN_TAGS = ['NP', 'NNP', 'NNS', 'NN', 'IN', 'POS', 'DT', 'CD']
+
+# Wh- words corresponding to different entity types
 WH_MAP = { 'PERSON': 'Who',
            'GPE': 'What',
            'LOC': 'Where',
@@ -20,21 +25,42 @@ class PredicateFinder:
         predicates = []
 
         for sentence in doc.sents:
+
+            # DEBUG
+            # print('SENTENCE:')
+            # print(sentence._.parse_string)
+            # print('\nCONSTITUENTS:')
+            # for child in sentence._.constituents:
+            #     print(child._.parse_string)
+            #     print('-')
+            # print('\n---\n')
+            # /DEBUG
+
             predicates.extend(self.find_predicates_in_sentence([], sentence))
 
+        # DEBUG
         # for pred in predicates:
-        #     print(pred.subj.text.strip(), end=' |')
+        #     print(pred.subj)
         #     print(pred.wh_word)
         #     print(pred.verb)
         #     print(pred.obj)
         #     print()
-        
+        # /DEBUG
+
         return predicates
 
     def find_predicates_in_sentence(self, predicates, root):
         # base case (also checked at every level)
         if self.is_predicate(root):
-            predicates.append(Predicate(root))
+            print(root)
+            print(root._.parse_string)
+            pred = Predicate(root)
+            print(pred.subj)
+            print(pred.wh_word)
+            print(pred.verb)
+            print(pred.obj)
+            print()
+            predicates.append(pred)
 
         # recursive case - check children for predicates
         for child in root._.children:
@@ -66,10 +92,10 @@ class Predicate:
     Holds information about a predicate for easy reconstruction.
     
     Properties:
-        subj: The subject of the predicate
-        wh_word: the 'wh' word corresponding to the predicate's subject
-        verb: The main verb of the predicate
-        obj: the object of the predicate
+        subj (list(Span)): The subject of the predicate
+        wh_word (str): the 'wh' word corresponding to the predicate's subject
+        verb (list(Span)): The main verb of the predicate
+        obj (Span): the object of the predicate
     """
 
     def __init__(self, sentence):
@@ -80,18 +106,38 @@ class Predicate:
         for i, child in enumerate(sentence._.children):
 
             if i == 0:
-                self.subj = self.__find_subject(child)
+                self.subj = self.__find_subject(child, [])
+                self.subj_ent = entity_from_span_lst(self.subj)
                 self.wh_word = self.__wh_word_from(self.subj)
 
             if i == 1:
                 self.verb = self.__find_verb(child, [])
                 self.obj = self.__find_object(child)
 
-    def __find_subject(self, root):
-        return get_entity(root)
+    def __find_subject(self, root, output):
+        # Recursive case: node still has children
+        # Reversing the children list to prevent titles
+        for child in list(root._.children)[::-1]:
+            # Not adding children before a title break
+            # TODO: Make this less janky
+            if constituent_tag(child._.parse_string) == '_SP':
+                break
+            
+            if is_noun(child):
+                self.__find_subject(child, output)
+        
+        # Base case: node has no children
+        if len(list(root._.children)) == 0:
+            # add the token if it fits the noun pattern
+            if constituent_tag(root._.parse_string) in NOUN_TAGS:
+                output.append(root)
+        
+        # un-reverse the list
+        return output[::-1]
     
-    def __wh_word_from(self, span):
-        return WH_MAP.get(span[0].ent_type_, 'What')
+    def __wh_word_from(self, lst):
+        # Get the corresponding wh word, defaults to 'What'
+        return WH_MAP.get(lst[0][0].ent_type_, 'What')
 
     def __find_verb(self, root, output):
         # base case: root is a verb
@@ -110,8 +156,6 @@ class Predicate:
     def __find_object(self, root):
         # FIXME: prepositions and other information-holding extenders
         #        are not yet accounted for.
-
-        print(root._.parse_string)
 
         # base case: root is a noun
         if is_noun(root):
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 '''Utility functions for question asking.'''
 
+import string
+
 def constituent_tag(parse_string):
     '''
     The PTB tag for the given constituent.
@@ -22,11 +24,45 @@ def is_verb_phrase(span):
 def is_noun(span):
     return constituent_tag(span._.parse_string).startswith('N')
 
-def get_entity(span):
+def entity_from_span(span):
     if span._.is_coref:
         return span._.coref_cluster.main
     else:
         return span
+
+def entity_from_span_lst(lst):
+    for span in lst:
+        if span._.is_coref:
+            return [span._.coref_cluster.main]
+    
+    return lst
+
+def str_from_token_lst(lst):
+    output = lst[0].text
+
+    num_quotes = 0
+
+    no_space_after = set(['$', '(', '[', '{', '-'])
+
+    no_space_before = set(string.punctuation) - set(['$', '(', '[', '{', '"'])
+
+    for i in range(1, len(lst)):
+        token = lst[i].text
+        last = lst[i-1].text
+
+        if token == '"':
+            num_quotes += 1
+
+            if num_quotes % 2 != 0:
+                output += ' '
+
+        elif last not in no_space_after and token[0] not in no_space_before:
+            if last != '"' or num_quotes % 2 == 0:
+                output += ' '
+
+        output += token
+
+    return output
 
 # Statements for debug
 if __name__ == '__main__':
